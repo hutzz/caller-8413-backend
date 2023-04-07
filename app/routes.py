c2661d26b6cb58a264307211ed46e57ae103f3f4
Zach <zachhutz0@gmail.com>
2023-04-07 12:11:37 -0400

    add path for model, disable for now because i like my vram

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from index import app
 from flask import request, jsonify
 from errno import errorcode
 from flask_mysqldb import MySQL
-from app import media, abbie, ram
+from app import media, abbie, ram, xi
 
 mysql = MySQL(app)
 
@@ -30,12 +30,17 @@ try:
         dl = media.mp3(url)
         if not dl: return jsonify('h')
         return jsonify(dl)
-    @app.route('/rammap', methods =['POST'])
+    @app.route('/rammap', methods = ['POST'])
     def rammap():
         data = request.get_json()
         mt1,mt2,cas1,cas2 = data 
         mt1,mt2,cas1,cas2 = int(mt1),int(mt2),int(cas1),int(cas2)
         return jsonify(ram.rammap(ram.megatransfers(mt1,mt2),ram.cas_latency(cas1,cas2)))
+    # @app.route('/xi')
+    # def iswinnie():
+    #     if xi.is_winnie_the_pooh():
+    #         return jsonify('0')
+    #     return jsonify('1')
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_BAD_DB_ERROR:
